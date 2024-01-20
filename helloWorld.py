659ad87f87607e14f799134cc1234ad5a4c9b3cc
Manav Mittal <123826237+ManavMittal12@users.noreply.github.com>
2024-01-20 17:00:17 +0530

    Add files via upload

---
+++ helloWorld.py
@@ -0,0 +1,29 @@
+
+print('hello world')
+# string literal is the sequence of characters enclosed in double or single quotes.
+
+print(1 +2)
+print(7 * 6)
+print()
+print('The end', 'or is it?', 'keep watching to learn more about Python', 3)
+
+
+# Jargons
+
+'''
+literal - a value of some type 
+
+function - a named block of code that we can call, by using it's name
+we can write our own functions  or we can call in the built in functions 
+
+arguments - a value passed to a function in order to give it values to work with There may be no arguments or there maybe be 1 
+or more
+
+
+calling a function - using the function name to execute the function's code
+
+return value the value that a function returns
+
+parameter also called formal parameter. 
+'''
+    
